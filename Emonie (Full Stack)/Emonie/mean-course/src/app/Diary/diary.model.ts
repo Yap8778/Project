ecb90cc26aa0063bb6883f22fbaddf48d1236895
Yap8778 <junsong1341@gmail.com>
2025-08-08 23:56:31 +0800

    s

--- Emonie (Full Stack)/Emonie/mean-course/src/app/Diary/diary.model.ts
+++
@@ -1,9 +0,0 @@
-export interface Diary {
-  _id?: string; // Optional field for the diary's unique identifier (used for updates or deletion)
-  title: string; // Title of the diary entry
-  content: string; // Content of the diary entry
-  date: Date; // Date of the diary entry (should be a valid Date object)
-  emotion: 'Joy' | 'Sadness' | 'Anger' | 'Anxiety' | 'Numb'; // Emotion associated with the diary entry, restricted to a set of predefined values
-}
- 
- 
